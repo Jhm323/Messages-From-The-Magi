d742c72bcd9ee785e120a87225eed700ca7f797f
holden <james.holden.moore@gmail.com>
2026-04-04 12:46:48 -0400

    conslidate cards.html & system.html into explore.html, add vercel.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,29 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    // Redirect legacy pages to their merged destination
+    middlewareMode: false,
   },
+  plugins: [
+    {
+      name: 'legacy-redirects',
+      configureServer(server) {
+        const REDIRECTS = {
+          '/cards.html':  '/explore.html',
+          '/system.html': '/explore.html',
+        };
+        server.middlewares.use((req, res, next) => {
+          const target = REDIRECTS[req.url?.split('?')[0]];
+          if (target) {
+            res.writeHead(301, { Location: target });
+            res.end();
+          } else {
+            next();
+          }
+        });
+      },
+    },
+  ],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
